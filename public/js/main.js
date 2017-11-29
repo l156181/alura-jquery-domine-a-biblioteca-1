25dbc9842464796fc69f38559075666c96fc21f7
Leandro Ferreira do Nascimento Junior <vntleju@venturus.local>
2017-11-29 14:28:35 -0200

    adding changes in the public folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,20 @@
 var tempoRestante = $("#tempo-digitacao").text();
 var tempoInicial = tempoRestante;
 var campo = $(".campo-digitacao");
-var botaoReiniciar = $("#botao-reiniciar")
-
-// $(document).ready(function(){
-//campo.css("background-color","lightgray");
+var botaoReiniciar = $("#botao-reiniciar");
+var frase = $(".frase").text();
+var intervalID;
 
 $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
     botaoReiniciar.click(reiniciaJogo);
+    $(".botao-remover").click(removeLinha);
 });
 
 function reiniciaJogo(){
+    campo.off("click");
     $("#botao-reiniciar").click(function(){
         campo.attr("disabled",false);
         campo.val("");
@@ -21,35 +22,37 @@ function reiniciaJogo(){
         $("#contador-caracteres").text("0");
         $("#tempo-digitacao").text(tempoInicial)
         inicializaCronometro();
+        campo.toggleClass("campo-desativado");
+        campo.removeClass("campo-correto campo-errado");
     });    
 }
 
-function atualizaTamanhoFrase(){
-    var frase = $(".frase").text();
+function atualizaTamanhoFrase(){    
     var arrayFrase = frase.split(" ");
     var numPalavras = arrayFrase.length;
-    
+
     var elementTamanhoFrase = $("#tamanho-frase");
     elementTamanhoFrase.text(numPalavras);
 }
 
 function inicializaCronometro(){
-    tempoRestante = tempoInicial;
-    campo.one("focus",function(){
-    var intervalID = setInterval(function(){
-            tempoRestante--;
-            $("#tempo-digitacao").text(tempoRestante);
-           
-            if(tempoRestante < 1){
-                campo.addClass("campo-desativado");
-                campo.attr("disabled",true);
-                clearInterval(intervalID);
-                $("#botao-reiniciar").attr("disabled",false); 
-            }   
-        },1000);
-    });
-
-    $("#botao-reiniciar").attr("disabled",true);
+    tempoRestante = tempoInicial;    
+    botaoReiniciar.attr("disabled",true);
+    
+    clearInterval(intervalID);
+    campo.off("focus");
+    
+    campo.one("focus",function(){   
+        intervalID = setInterval(function(){
+             tempoRestante--;
+             $("#tempo-digitacao").text(tempoRestante);
+            
+             if(tempoRestante < 1){
+                 clearInterval(intervalID);
+                 finalizaJogo();
+             }   
+         },1000);
+     });
 }
 
 function inicializaContadores(){
@@ -58,5 +61,30 @@ function inicializaContadores(){
         var qtdPalavras = conteudo.split(/\s+/).length;
         $("#contador-palavras").text(qtdPalavras);
         $("#contador-caracteres").text(conteudo.length);
+
+        var comparavel = frase.substr(0,conteudo.length);
+        
+        if(comparavel == conteudo){
+            campo.addClass("campo-correto");
+            campo.removeClass("campo-errado");
+        }else{
+            campo.addClass("campo-errado");
+            campo.removeClass("campo-correto");
+        }
     });
 }
+
+function finalizaJogo(){
+    campo.addClass("campo-desativado");
+    campo.attr("disabled",true);
+    botaoReiniciar.attr("disabled",false); 
+    inserePlacar();
+}
+
+
+// $(document).ready(function(){
+// campo.css("background-color","lightgray");
+// campo.addClass("campo-desativado");
+// frase.startsWith(digitado)
+// append or prepend
+// .hide();
